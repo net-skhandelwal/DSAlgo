690f7cf46bc61fea16d901fb393f5375b53014da
Shashank Khandelwal <Shashank.11609001@lpu.in>
2021-04-04 12:47:50 +0530

    Reversed Array using recursion approach

--- DSAlgoCSharp/Array/ReverseArray.cs
+++ DSAlgoCSharp/Array/ReverseArray.cs
@@ -27,5 +27,31 @@ namespace DSAlgoCSharp.Array
             }
             Console.WriteLine($"Output: [{string.Join(", ", array)}]");
         }
+
+        public void RevArrayRecursion(int n)
+        {
+            var array = _data.GetInputArray(n);
+            Console.WriteLine($"Input: [{string.Join(", ", array)}]");
+            int maxIndex = n;
+            int minIndex = 0;
+            RevArrRec(minIndex, maxIndex,ref array);
+            Console.WriteLine($"Output: [{string.Join(", ", array)}]");
+        }
+
+        private void RevArrRec(int minIndex, int maxIndex,ref int[] array)
+        {
+            if(minIndex < (int)(maxIndex/2))
+            {
+                array[minIndex] = array[minIndex] + array[maxIndex - minIndex - 1];
+                array[maxIndex - minIndex - 1] = array[minIndex] - array[maxIndex - minIndex - 1];
+                array[minIndex] = array[minIndex] - array[maxIndex - minIndex - 1];
+                minIndex++;
+                RevArrRec(minIndex, maxIndex,ref array);
+            }
+            else
+            {
+                return;
+            }
+        }
     }
 }
